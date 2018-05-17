69451359f4370e4d898d5a6eccdf825937144713
zhaohailing1 <291330512@qq.com>
2018-05-17 10:38:31 +0800

    #init 1.0.0

--- models/UserModel.class.php
+++ models/UserModel.class.php
@@ -1,6 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/../helper/ActiveRecord.php';
 require_once dirname(__FILE__) . '/../conf/db_init.php';
+
 // assumes a table named "books" with a pk named "id"
 // see simple.sql
 class UserModel extends ActiveRecord\Model { 
